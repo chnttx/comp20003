64c8e9cb18692ff7b0835a0a4913682e3a131e17
harrynguyen864 <n11t2h79ette@gmail.com>
2023-08-11 13:43:54 +1000

    fixed data.c

--- assignment1/data.c
+++ assignment1/data.c
@@ -4,8 +4,3 @@
 #include <string.h>
 #include "data.h"
 
-struct restaurant {
-    int census_year, block_id, property_id, base_property_id, industry_code, num_seats;
-    double longitude, latitude;
-    char *bld_add, *clue_small_area, *business_add, *trading_name, *description, *seating;
-};
