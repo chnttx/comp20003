f1754bf5ac25b769d885196f992456cc38416b0f
harrynguyen864 <n11t2h79ette@gmail.com>
2023-08-12 15:17:57 +1000

    fixed data.c

--- assignment1/data.c
+++ assignment1/data.c
@@ -3,8 +3,11 @@
 #include <assert.h>
 #include <string.h>
 #include "data.h"
-
+#include "lists.h"
 char *getName(restaurant_t restaurant) {
     return restaurant.trading_name;
 }
 
+void printCount(char* name, list_t *lst) {
+    printf("%s --> %d\n", name, findRestaurant(lst, name));
+}
