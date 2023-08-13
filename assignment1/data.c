8d3c26084de95180a6e7ffac9b8147342538abb4
harrynguyen864 <n11t2h79ette@gmail.com>
2023-08-13 20:09:29 +1000

    added driver.c

--- assignment1/data.c
+++ assignment1/data.c
@@ -11,3 +11,16 @@ char *getName(restaurant_t restaurant) {
 void printCount(char* name, list_t *lst) {
     printf("%s --> %d\n", name, findRestaurant(lst, name));
 }
+
+void skipHeaderLine(FILE *f) {
+    while (fgetc(f) != '\n');
+}
+
+restaurant_t *resRead(FILE *f) {
+    restaurant_t *res = NULL;
+    int year, buildingID, propertyID, basePropertyID, industryCode, seats;
+    double longitudeX, latitudeY;
+    char *bldAdd, *clueSmallArea, *busiAdd, *name, *desc, *seatType;
+    char *currLine = NULL; 
+    
+}
--- assignment1/driver.c
+++ assignment1/driver.c
@@ -2,14 +2,33 @@
 #include <string.h>
 #include <assert.h>
 #include <stdlib.h>
+#include "data.h"
+#include "lists.h"
+
+list_t *buildRestaurantList(char *);
 
 int main(int argc, char **argv) {
-    if (argc < 4) {
-        exit(EXIT_FAILURE);
-    }
 
-    FILE *inFile = fopen(argv[2], "r"), *outFile = fopen(argv[3], "w");
-    assert(inFile && outFile);
+    FILE *infoFile = fopen(argv[2], "w");
+    assert(infoFile);
 
-    
+    fclose(infoFile);
+
+    return 0;
+
 }
+
+list_t *buildRestaurantList(char* filename) {
+    FILE *f = fopen(filename, "r");
+    assert(f);
+
+    list_t *restaurants = listCreate();
+    skipHeader(f);
+    restaurant_t *res;
+    while ((res = getRestaurant(f))) {
+        listAppend(restaurants, res);
+    }
+    fclose(f);
+    return restaurants;
+}
+
