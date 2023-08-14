7d40eb1954a26874e7cb79cf9de1ff80cf85c75d
harrynguyen864 <n11t2h79ette@gmail.com>
2023-08-14 18:46:17 +1000

    fixed driver.c and data.h

--- assignment1/driver.c
+++ assignment1/driver.c
@@ -2,48 +2,47 @@
 #include <string.h>
 #include <assert.h>
 #include <stdlib.h>
-// #include "data.h"
-// #include "lists.h"
+#include "data.h"
+#include "lists.h"
 
-// list_t *buildRestaurantList(char *);
+list_t *buildRestaurantList(char *);
 void listQuerying();
 
 int main(int argc, char **argv) {
 
-    // FILE *infoFile = fopen(argv[3], "w");
-    // assert(infoFile);
+    FILE *infoFile = fopen(argv[3], "w");
+    assert(infoFile);
 
-    // listQuerying(argv[2], stdin, stdout, infoFile);
-    listQuerying();
+    listQuerying(argv[2], stdin, stdout, infoFile);
 
-    // fclose(infoFile);
+    fclose(infoFile);
 
     return 0;
 
 }
 
-// list_t *buildRestaurantList(char* filename) {
-//     FILE *f = fopen(filename, "r");
-//     assert(f);
+list_t *buildRestaurantList(char* filename) {
+    FILE *f = fopen(filename, "r");
+    assert(f);
 
-//     list_t *restaurants = listCreate();
-//     skipHeader(f);
-//     restaurant_t *res;
+    list_t *restaurants = listCreate();
+    skipHeader(f);
+    restaurant_t *res;
 
-//     while ((res = getRestaurant(f))) {
-//         listAppend(restaurants, res);
-//     }
+    while ((res = getRestaurant(f))) {
+        listAppend(restaurants, res);
+    }
 
-//     fclose(f);
-//     return restaurants;
-// }
+    fclose(f);
+    return restaurants;
+}
 
-void listQuerying() {
+void listQuerying(char *dataFilename, FILE *inFile, FILE *outFile, FILE *infoFile) {
 
-    // list_t *restaurants = buildRestaurantList(dataFileName);
+    list_t *restaurants = buildRestaurantList(dataFilename);
 
     char *searchQuery;
-    // restaurant_t *r;
+    restaurant_t *r;
 
     size_t bufferSize = 0;
     __ssize_t charactersRead;
